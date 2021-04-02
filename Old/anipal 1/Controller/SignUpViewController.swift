925e280b0d391e47e46140f2573a5b21a49b742b
Joon <kjo11300@naver.com>
2021-04-03 02:32:26 +0900

    feat : 개인정보 페이지 생성
    이름, 생년월일, 성별 선택 페이지 생성

--- Old/anipal 1/Controller/SignUpViewController.swift
+++ Old/anipal 1/Controller/SignUpViewController.swift
@@ -9,39 +9,87 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    @IBOutlet var logoView: UIImageView!
+    @IBOutlet var dateField: UITextField!
+    @IBOutlet var genderChoice: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Make logoView Circle
+        logoView.layer.borderWidth = 1
+        logoView.layer.masksToBounds = false
+        logoView.layer.borderColor = UIColor.gray.cgColor
+        logoView.layer.cornerRadius = logoView.frame.height/2
+        logoView.clipsToBounds = true
 
+        self.dateField.setInputViewDatePicker(target: self, selector: #selector(tapDone))
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        navigationController?.isNavigationBarHidden = false
-    }
-
-//    @IBAction func backBtn(_ sender: UIButton) {
-//        self.presentingViewController?.dismiss(animated: true, completion: nil)
-//    }
-//
-//    @IBAction func nextBtn(_ sender: UIButton) {
-//
-//        guard let nextVC = self.storyboard?.instantiateViewController(identifier: "signupVC2") else {
-//            return
-//        }
-//
-//        nextVC.modalTransitionStyle = .coverVertical
-//        nextVC.modalPresentationStyle = .fullScreen
-//        self.present(nextVC, animated: true)
-//
-//    }
-    
-    @IBAction func nextBarBtn(_ sender: UIBarButtonItem) {
+    @IBAction func nextPageButton(_ sender: UIButton) {
         guard let nextVC = self.storyboard?.instantiateViewController(identifier: "signupVC2") else {
             return
         }
-
-        nextVC.modalTransitionStyle = .coverVertical
-        nextVC.modalPresentationStyle = .fullScreen
         self.navigationController?.pushViewController(nextVC, animated: true)
     }
+    
+    // For datePicker
+    @objc func tapDone() {
+        if let datePicker = self.dateField.inputView as? UIDatePicker {
+            let dateformatter = DateFormatter()
+            dateformatter.dateStyle = .medium
+            self.dateField.text = dateformatter.string(from: datePicker.date)
+        }
+        self.dateField.resignFirstResponder()
+    }
+   
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.isNavigationBarHidden = false
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+    
+    }
+    
+    @IBAction func cancelBarButton(_ sender: UIBarButtonItem) {
+        self.navigationController?.popToRootViewController(animated: true)
+    }
 
+}
+
+// MARK: - 데이트피커 텍스트필드안에 넣기
+extension UITextField {
+    func setInputViewDatePicker(target: Any, selector: Selector) {
+        // Create a UIDatePicker object and assign to inputView
+        let screenWidth = UIScreen.main.bounds.width
+        let datePicker = UIDatePicker(frame: CGRect(x: 0, y: 0, width: screenWidth, height: 216))
+        datePicker.datePickerMode = .date
+        // iOS 14 and above
+        if #available(iOS 14, *) {// Added condition for iOS 14
+            datePicker.preferredDatePickerStyle = .wheels
+            datePicker.sizeToFit()
+                
+            let date = DateFormatter()
+            date.locale = Locale(identifier: "ko_kr")
+            date.dateFormat = "yyyy-MM-dd"
+            
+            // 최소, 최대 년도 설정
+            let maxTime = date.date(from: "2017-01-01")
+            let minTime = date.date(from: "1990-01-01")
+            datePicker.maximumDate = maxTime
+            datePicker.minimumDate = minTime
+        }
+        self.inputView = datePicker
+        // Create a toolbar and assign it to inputAccessoryView
+        let toolBar = UIToolbar(frame: CGRect(x: 0.0, y: 0.0, width: screenWidth, height: 44.0))
+        let flexible = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let cancel = UIBarButtonItem(title: "Cancel", style: .plain, target: nil, action: #selector(tapCancel))
+        let barButton = UIBarButtonItem(title: "Done", style: .plain, target: target, action: selector)
+        toolBar.setItems([cancel, flexible, barButton], animated: false)
+        self.inputAccessoryView = toolBar
+    }
+    
+    @objc func tapCancel() {
+        self.resignFirstResponder()
+    }
 }
