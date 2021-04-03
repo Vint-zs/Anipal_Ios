609dfde470dbd942dbe76a544b8c9e4f8f4e1232
Joon <kjo11300@naver.com>
2021-04-04 01:45:22 +0900

    feat: 언어 선택 테이블 뷰 생성
    언어 선택 테이블뷰 생성 및 테이블내용 추가

--- Old/anipal 1/Controller/SingUpViewController2.swift
+++ Old/anipal 1/Controller/SingUpViewController2.swift
@@ -9,23 +9,50 @@ import UIKit
 
 class SingUpViewController2: UIViewController {
 
+    @IBOutlet var languageTableView: UITableView!
+    let languageList = ["English", "한국어", "日本語", "中文", "Italiano"]
+    let levelList = ["초급", "중급", "고급"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        languageTableView.delegate = self
+        languageTableView.dataSource = self
+        
     }
     
-    @IBAction func nextBtn(_ sender: UIButton) {
-        
-        guard let loginVC = self.storyboard?.instantiateViewController(identifier: "GoogleLogin") else {
+    @IBAction func nextPageButton(_ sender: UIButton) {
+        guard let nextVC = self.storyboard?.instantiateViewController(identifier: "signupVC3") else {
             return
         }
-        
-        navigationController?.pushViewController(loginVC, animated: true)
-//        loginVC.modalTransitionStyle = .coverVertical
-//        loginVC.modalPresentationStyle = .fullScreen
-//        self.present(loginVC, animated: true)
-        
+        self.navigationController?.pushViewController(nextVC, animated: true)
     }
     
+    @IBAction func cancelBarButton(_ sender: UIBarButtonItem) {
+        self.navigationController?.popToRootViewController(animated: true)
+    }
+
+}
+extension SingUpViewController2: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return languageList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "languageCell", for: indexPath) as? LanguageTableViewCell else {
+            return UITableViewCell()
+        }
+        cell.languageName.text = languageList[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
 }
