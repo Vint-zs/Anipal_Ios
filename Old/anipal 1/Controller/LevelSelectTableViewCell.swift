8dea3d6484569552ed0305efa3488af584db54f8
Joon <kjo11300@naver.com>
2021-04-04 03:07:24 +0900

    feat : 레벨선택뷰셀 생성
    초,중,고급 버튼을 담을 뷰 셀 생성 및 버튼객체 생성

---
+++ Old/anipal 1/Controller/LevelSelectTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  LevelSelectTableViewCell.swift
+//  anipal 1
+//
+//  Created by Kim JoonOh on 2021/04/04.
+//
+
+import UIKit
+
+class LevelSelectTableViewCell: UITableViewCell {
+
+    @IBOutlet var levelButton: UIButton!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
