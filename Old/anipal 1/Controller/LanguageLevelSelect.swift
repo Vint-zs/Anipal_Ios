6b97c245e92c724ef00f0e1f65e74a69548b01f0
Joon <kjo11300@naver.com>
2021-04-04 13:16:01 +0900

    style : 언어버튼 스타일 수정
    언어버튼 둥글게 수정 및 셀 높이 수정

--- Old/anipal 1/Controller/LanguageLevelSelect.swift
+++ Old/anipal 1/Controller/LanguageLevelSelect.swift
@@ -9,12 +9,15 @@ import UIKit
 
 class LanguageLevelSelect: UIViewController {
 
+    @IBOutlet var tableView: UITableView!
+    
     let levelList = ["초급", "중급", "고급"]
     @IBOutlet var levelTable: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         levelTable.delegate = self
         levelTable.dataSource = self
+        
     }
 
 }
@@ -30,8 +33,18 @@ extension LanguageLevelSelect: UITableViewDelegate, UITableViewDataSource {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "levelSelectCell", for: indexPath) as? LevelSelectTableViewCell else {
             return UITableViewCell()
         }
-        cell.levelButton.layer.cornerRadius = 5
-        cell.levelButton.setTitle(levelList[indexPath.row], for: .normal)
+        
+        cell.levelLabel.text = levelList[indexPath.row]
+        cell.levelLabel.layer.masksToBounds = true
+        cell.levelLabel.layer.cornerRadius = 10
+        //cell.selectionStyle = .none
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.presentingViewController?.dismiss(animated: true, completion: nil)
     }
 }
