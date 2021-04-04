9f8c68664bfb3790404a959785a078b7bc0f08ea
isabellajoo <dmdkdmdkdkdk@gmail.com>
2021-04-04 21:23:30 +0900

    style: 관심사를 Model/favorites로 이동

--- Old/anipal 1/View/CustomView/FavoriteView.swift
+++ Old/anipal 1/View/CustomView/FavoriteView.swift
@@ -15,23 +15,6 @@ protocol FavoriteViewDelegate {
 class FavoriteView: UIView, UICollectionViewDataSource {
     public var delegate: FavoriteViewDelegate?
     
-    let favorites = [
-        ["text": "🍿Movie".localized, "data": "영화"],
-        ["text": "✈️Travel".localized, "data": "여행"],
-        ["text": "🎤Sing".localized, "data": "노래"],
-        ["text": "💪Fitness".localized, "data": "피트니스"],
-        ["text": "🎨Design".localized, "data": "디자인"],
-        ["text": "🍳Cook".localized, "data": "요리"],
-        ["text": "🕺Dance".localized, "data": "춤"],
-        ["text": "📚Book".localized, "data": "독서"],
-        ["text": "💅Beauty".localized, "data": "미용"],
-        ["text": "🛍Shopping".localized, "data": "쇼핑"],
-        ["text": "🎮Game".localized, "data": "게임"],
-        ["text": "💻IT".localized, "data": "IT"],
-        ["text": "🏛Architecture".localized, "data": "건축"],
-        ["text": "💰Economy".localized, "data": "경제"]
-    ]
-    
     @IBOutlet var favView: UIView!
     @IBOutlet weak var favCell: UICollectionView!
     
