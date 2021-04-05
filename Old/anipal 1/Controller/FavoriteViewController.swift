65faca331e1f327436ae3a458cf538a1cbacd7c2
isabellajoo <dmdkdmdkdkdk@gmail.com>
2021-04-05 11:28:11 +0900

    feat: 회원가입 관심사 title 추가

--- Old/anipal 1/Controller/FavoriteViewController.swift
+++ Old/anipal 1/Controller/FavoriteViewController.swift
@@ -9,7 +9,10 @@ import UIKit
 
 class FavoriteViewController: UIViewController {
     
+    @IBOutlet weak var favLabelTitle: UILabel!
+    
     override func viewDidLoad() {
+        favLabelTitle.text = "Choose your favorites.".localized
         super.viewDidLoad()
     }
 }
