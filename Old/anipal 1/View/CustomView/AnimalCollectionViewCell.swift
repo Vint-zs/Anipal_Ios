aba501b9f7471a37441473af4ba482f5474bd97e
Joon <kjo11300@naver.com>
2021-04-05 21:15:39 +0900

    feat : 동물선택 뷰셀 생성
    동물선택 뷰셀 xib파일 및 셀 생성

---
+++ Old/anipal 1/View/CustomView/AnimalCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  AnimalCollectionViewCell.swift
+//  anipal 1
+//
+//  Created by Kim JoonOh on 2021/04/05.
+//
+
+import UIKit
+
+class AnimalCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet var img: UIImageView!
+    @IBOutlet var name: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
