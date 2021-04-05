2082d8c6961e5ac1409d8491439135770204c400
Joon <kjo11300@naver.com>
2021-04-06 00:42:07 +0900

    feat : 터치효과 제거
    셀 터치효과 제거

--- Old/anipal 1/Controller/LanguageTableViewCell.swift
+++ Old/anipal 1/Controller/LanguageTableViewCell.swift
@@ -15,6 +15,7 @@ class LanguageTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        selectionStyle = .none
         // Initialization code
     }
 
