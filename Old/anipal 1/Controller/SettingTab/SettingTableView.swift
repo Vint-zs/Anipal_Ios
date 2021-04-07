ec0c54dbc5ef089afde142e11893ad229c3c518e
isabellajoo <dmdkdmdkdkdk@gmail.com>
2021-04-08 01:15:53 +0900

    feat: Tab4 이름을 SettingTab으로 변경

--- Old/anipal 1/Controller/SettingTab/SettingTableView.swift
+++ Old/anipal 1/Controller/SettingTab/SettingTableView.swift
@@ -1,5 +1,5 @@
 //
-//  SettingPageTableViewCell.swift
+//  SettingTableViewCell.swift
 //  anipal 1
 //
 //  Created by 이예주 on 2021/03/31.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class SettingPageTableViewCell: UITableViewCell {
+class SettingTableView: UITableViewCell {
     
     @IBOutlet weak var settingLabel: UILabel!
     
