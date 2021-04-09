a07e81198f0098ed166470b1a92412d3ab08a3e0
isabellajoo <dmdkdmdkdkdk@gmail.com>
2021-04-10 00:41:02 +0900

    style: LetterListViewController 주석 삭제

--- Old/anipal 1/Controller/LetterListTab/LetterListViewController.swift
+++ Old/anipal 1/Controller/LetterListTab/LetterListViewController.swift
@@ -72,38 +72,3 @@ extension LetterListViewController {
         }
     }
 }
-
-// extension LetterListViewController: UITableViewDelegate, UITableViewDataSource {
-//
-//    // cell 높이
-//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-//        return 100
-//    }
-//
-//    // cell 수
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return letters.count
-//    }
-//
-//    // cell 내용
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell2", for: indexPath) as! LetterListTableViewCell
-//        cell.userName.text = letters[indexPath.row].fromLetter
-//        cell.userImg.image = letters[indexPath.row].imgInit
-//        return cell
-//    }
-//
-//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//
-//        guard let writingVC = self.storyboard?.instantiateViewController(identifier: "letterDetail")as? LetterDetailViewController else {
-//            return
-//        }
-//
-//        writingVC.nameFromVar = letters[indexPath.row].fromLetter
-//        writingVC.contentVar = letters[indexPath.row].contentInit
-//
-//        self.navigationController?.pushViewController(writingVC, animated: true)
-//    }
-//
-//}
