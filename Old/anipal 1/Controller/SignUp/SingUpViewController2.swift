f94e95fda9bd81a34109960652e70f4369365e3d
Joon <kjo11300@naver.com>
2021-04-11 20:52:49 +0900

    feat : 언어 레벨선택화면
    언어 선택화면 체크박스 작업 및 레벨 선택 작업 완료

--- Old/anipal 1/Controller/SignUp/SingUpViewController2.swift
+++ Old/anipal 1/Controller/SignUp/SingUpViewController2.swift
@@ -12,21 +12,31 @@ class SingUpViewController2: UIViewController {
     @IBOutlet var titleLabel: UILabel!
     @IBOutlet var languageTableView: UITableView!
     @IBOutlet var searchBar: UISearchBar!
+//    let searchController = UISearchController(searchResultsController: nil)
     
-    let languageList = ["English", "한국어", "日本語", "中文", "Italiano"]
-    
+    let languageList = ["English", "한국어", "日本語", "中文", "Italiano","프랑스어","포르투갈어"]
+    var myLanguageList: [String:Int] = ["한국어":1,"프랑스어":3]
+
     override func viewDidLoad() {
         super.viewDidLoad()
         languageTableView.delegate = self
         languageTableView.dataSource = self
-        // titleLabel.font = UIFont(name: "NotoSansKR-Bold", size: 18)
+//        titleLabel.font = UIFont(name: "NotoSansKR-Bold", size: 18)
         titleLabel.textColor = UIColor(red: 0.392, green: 0.392, blue: 0.392, alpha: 1)
+        
+//        searchController.searchResultsUpdater = self
+//        searchController.obscuresBackgroundDuringPresentation = false
+//        searchController.searchBar.placeholder = "search Keyword"
+//        navigationItem.searchController = searchController
+
     }
     
     @IBAction func nextPageButton(_ sender: UIButton) {
         guard let nextVC = self.storyboard?.instantiateViewController(identifier: "conceptSelectVC") else {
             return
         }
+        ad?.languages = myLanguageList
+        print(ad?.languages)
         self.navigationController?.pushViewController(nextVC, animated: true)
     }
     
@@ -49,13 +59,35 @@ extension SingUpViewController2: UITableViewDelegate, UITableViewDataSource {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "languageCell", for: indexPath) as? LanguageTableViewCell else {
             return UITableViewCell()
         }
+        
         cell.languageName.text = languageList[indexPath.row]
         cell.languageName.textColor = UIColor.init(red: 0.392, green: 0.392, blue: 0.392, alpha: 1)
+        cell.languageLevel.textColor = UIColor.init(red: 0.392, green: 0.392, blue: 0.392, alpha: 1)
+        // cell.languageLevel.text = String(level)
         // cell.languageName.font = UIFont(name: "Helvetica-Bold", size: 18)
         
-        cell.checkBox.layer.borderWidth = 2
+        cell.checkBox.layer.borderWidth = 1
         cell.checkBox.layer.borderColor = UIColor.init(red: 0.392, green: 0.392, blue: 0.392, alpha: 1).cgColor
         cell.checkBox.layer.cornerRadius = 5
+        cell.checkBox.clipsToBounds = true
+        cell.checkBox.backgroundColor = .white
+        
+        if myLanguageList.keys.contains(languageList[indexPath.row]) {
+            cell.checkBox.image = #imageLiteral(resourceName: "체크박스-1")
+            let level = myLanguageList[languageList[indexPath.row]]!
+            if level == 1 {
+                cell.languageLevel.text = "Beginner".localized
+            } else if level == 2 {
+                cell.languageLevel.text = "Intermediate".localized
+            } else {
+                cell.languageLevel.text = "Advanced".localized
+            }
+            
+        } else {
+            cell.checkBox.image = #imageLiteral(resourceName: "빈체크박스-1")
+            cell.languageLevel.text = ""
+        }
+        
         return cell
     }
     
@@ -64,9 +96,38 @@ extension SingUpViewController2: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        guard let nextVC = self.storyboard?.instantiateViewController(identifier: "levelSelectVC") else {
-            return
+
+        if myLanguageList.keys.contains(languageList[indexPath.row]) {
+            myLanguageList.removeValue(forKey: languageList[indexPath.row])
+            print(myLanguageList)
+            self.languageTableView.reloadData()
+        } else {
+            
+            let alertcontroller = UIAlertController(title: "Level".localized, message: nil, preferredStyle: .actionSheet)
+            let basicBtn = UIAlertAction(title: "Beginner".localized, style: .default) { (_) in
+                self.myLanguageList[self.languageList[indexPath.row]] = 1
+                self.languageTableView.reloadData()
+            }
+            let mediumBtn = UIAlertAction(title: "Intermediate".localized, style: .default) { (_) in
+                self.myLanguageList[self.languageList[indexPath.row]] = 2
+                self.languageTableView.reloadData()
+            }
+            let intermediateBtn = UIAlertAction(title: "Advanced".localized, style: .default) { (_) in
+                self.myLanguageList[self.languageList[indexPath.row]] = 3
+                self.languageTableView.reloadData()
+            }
+            
+            let cancelBtn = UIAlertAction(title: "Cancel".localized, style: .cancel, handler: nil)
+            alertcontroller.addAction(basicBtn)
+            alertcontroller.addAction(mediumBtn)
+            alertcontroller.addAction(intermediateBtn)
+            alertcontroller.addAction(cancelBtn)
+            present(alertcontroller, animated: true, completion: nil)
         }
-        self.present(nextVC, animated: true, completion: nil)
     }
+}
+
+extension SingUpViewController2: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+      }
 }
