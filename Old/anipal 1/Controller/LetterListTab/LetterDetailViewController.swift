a71978cc9e7e91d929cfe30b1c2c130abf75a4f8
isabellajoo <dmdkdmdkdkdk@gmail.com>
2021-04-15 00:51:00 +0900

    feat: 내게 온 편지 ui 수정
    보낸 사용자 정보 나타내는 ui 수정

--- Old/anipal 1/Controller/LetterListTab/LetterDetailViewController.swift
+++ Old/anipal 1/Controller/LetterListTab/LetterDetailViewController.swift
@@ -9,20 +9,37 @@ import UIKit
 
 class LetterDetailViewController: UIViewController {
 
-//    @IBOutlet weak var nameFrom: UILabel!
-    @IBOutlet weak var textviewContent: UITextView!
+    @IBOutlet weak var topView: UIView!
+    @IBOutlet weak var textViewContent: UITextView!
     @IBOutlet weak var menuBtn: UIBarButtonItem!
+    @IBOutlet weak var senderName: UILabel!
+    @IBOutlet weak var senderCountry: UILabel!
+    @IBOutlet weak var senderFav: UILabel!
+    @IBOutlet weak var senderAnimal: UIImageView!
     
     var nameFromVar: String?
     var contentVar: String?
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // 네비게이션
         self.navigationItem.title = "Letter".localized
         self.navigationItem.rightBarButtonItem = menuBtn
         
-
-//        nameFrom.text = nameFromVar
-        textviewContent.text = contentVar
+        // 보낸 사용자 정보
+        topView.layer.backgroundColor = UIColor(red: 0.946, green: 0.946, blue: 0.946, alpha: 1).cgColor
+        
+        senderName.text = nameFromVar
+        senderName.sizeToFit()
+        
+        senderAnimal.backgroundColor = .white
+        senderAnimal.layer.cornerRadius = senderAnimal.frame.height/2
+        senderAnimal.layer.borderWidth = 0.3
+        senderAnimal.layer.borderColor = UIColor.lightGray.cgColor
+        
+        // 편지 내용
+        textViewContent.layer.cornerRadius = 10
+        textViewContent.text = contentVar
     }
     
     @IBAction func writeBtn(_ sender: UIButton) {
