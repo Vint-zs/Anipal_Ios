20b47b1548f81294e4fe4168b2c924cdd8770614
isabellajoo <dmdkdmdkdkdk@gmail.com>
2021-05-11 02:29:03 +0900

    feat: 설정 탭 대표 동물 선택 기능 구현

--- Old/anipal 1/Controller/SettingTab/SettingTab.swift
+++ Old/anipal 1/Controller/SettingTab/SettingTab.swift
@@ -8,27 +8,58 @@
 import UIKit
 import GoogleSignIn
 import FBSDKLoginKit
+import SwiftyJSON
 
-class SettingTab: UIViewController {
-    @IBOutlet var favImage: UIImageView!
+class SettingTab: UIViewController, sendBackDelegate {
     
-    let settings: [String] = ["User info".localized, "Language".localized,  "Favorite".localized]
+    let settings: [String] = ["Language".localized, "Favorite".localized]
+    let sections: [String] = ["Language".localized, "Favorite".localized]
     
-    let sections: [String] = ["Language".localized,  "Favorite".localized]
-
-    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var favBtn: UIButton!
     @IBOutlet weak var settingTableView: UITableView!
-    
     @IBOutlet weak var logoutBtn: UIButton!
+    
+    var selectedAnimal: Int = 0
+    
+    var animals: [AnimalPost] = []  // 서버 POST용
+    var serverAnimals: [Animal] = [] // next page 데이터 전송용
+    var images: [UIImage] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         logoutBtn.layer.cornerRadius = 10
         logoutBtn.setTitle("Logout".localized, for: .normal)
         
-        self.settingTableView.tableFooterView = UIView(frame: .zero)
+        // 동물 선택 버튼
+        favBtn.backgroundColor = .white
+        favBtn.layer.cornerRadius = favBtn.frame.height/2
+        favBtn.layer.borderWidth = 0.3
+        favBtn.layer.borderColor = UIColor.lightGray.cgColor
         
-        // Do any additional setup after loading the view.
+        self.settingTableView.tableFooterView = UIView(frame: .zero)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        loadAnimal()
+    }
+    
+    func dataReceived(data: Int) {
+        selectedAnimal = data
+        favBtn.setImage(animals[data].animalImg, for: .normal)
+        favBtn.imageView?.contentMode = .scaleAspectFit
+    }
+    
+    // 대표 동물 변경
+    @IBAction func editAnimal(_ sender: UIButton) {
+        let sub = UIStoryboard(name: "SignUp", bundle: nil)
+        guard let nextVC = sub.instantiateViewController(identifier: "selectAnimalVC") as? SelectAnimal else {
+            return
+        }
+        
+        nextVC.delegate = self
+        nextVC.serverAnimals = self.serverAnimals
+        
+        self.present(nextVC, animated: true, completion: nil)
     }
     
     @IBAction func clickLogoutBtn(_ sender: UIButton) {
@@ -40,7 +71,78 @@ class SettingTab: UIViewController {
         ad?.favorites = []
         ad?.languages = []
         ad?.favAnimal = ""
-        
+    }
+    
+    func loadAnimal() {
+        if let session = HTTPCookieStorage.shared.cookies?.filter({$0.name == "Authorization"}).first {
+            get(url: "/own/animals", token: session.value, completionHandler: { [self] data, response, error in
+                guard let data = data, error == nil else {
+                    print("error=\(String(describing: error))")
+                    return
+                }
+                
+                if let httpStatus = response as? HTTPURLResponse {
+                    if httpStatus.statusCode == 200 {
+                        for idx in 0..<JSON(data).count {
+                            let json = JSON(data)[idx]
+                            let animalURLs: [String: String] = [
+                                "animal_url": json["animal_url"].stringValue,
+                                "head_url": json["head_url"].stringValue,
+                                "top_url": json["top_url"].stringValue,
+                                "pants_url": json["pants_url"].stringValue,
+                                "shoes_url": json["shoes_url"].stringValue,
+                                "gloves_url": json["gloves_url"].stringValue
+                            ]
+                            let animalImg = loadAnimals(urls: animalURLs)
+                            let comingAnimal = [
+                                "animal_url": json["coming_animal"]["animal_url"].stringValue,
+                                "bar": json["coming_animal"]["bar"].stringValue,
+                                "background": json["coming_animal"]["background"].stringValue
+                            ]
+                            let animal = AnimalPost(animal: json["animal"]["localized"].stringValue, animalURLs: animalURLs, isUsed: json["is_used"].boolValue, delayTime: json["delay_time"].stringValue, comingAnimal: comingAnimal, animalImg: animalImg, ownAnimalId: json["_id"].stringValue)
+                            animals.append(animal)
+                            serverAnimals.append(Animal(nameInit: json["animal"]["localized"].stringValue, image: animalImg))
+                        }
+                    } else if httpStatus.statusCode == 400 {
+                        print("error: \(httpStatus.statusCode)")
+                    } else {
+                        print("error: \(httpStatus.statusCode)")
+                    }
+                }
+            })
+        }
+    }
+    
+    // MARK: - 이미지 합성
+    func loadAnimals(urls: [String: String]) -> UIImage {
+        let order = urls.sorted(by: <)
+        images = []
+        for (_, url) in order {
+            setImage(from: url)
+        }
+        return compositeImage(images: images)
+    }
+    
+    func compositeImage(images: [UIImage]) -> UIImage {
+        var compositeImage: UIImage!
+        if images.count > 0 {
+            let size: CGSize = CGSize(width: images[0].size.width, height: images[0].size.height)
+            UIGraphicsBeginImageContext(size)
+            for image in images {
+                let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+                image.draw(in: rect)
+            }
+            compositeImage = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+        }
+        return compositeImage
+    }
+    
+    func setImage(from url: String) {
+        guard let imageURL = URL(string: url) else { return }
+        guard let imageData = try? Data(contentsOf: imageURL) else { return }
+        let image = UIImage(data: imageData)
+        self.images.append(image!)
     }
 }
 
