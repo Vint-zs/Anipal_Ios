de7f94489fee92c6e14bb394eb0a92037042d494
Joon <kjo11300@naver.com>
2021-05-16 16:39:12 +0900

    feat : accessory struct 생성
    악세서리 표출시 사용할 struct 생성

---
+++ Old/anipal 1/Model/Accessory.swift
@@ -0,0 +1,16 @@
+//
+//  Accessory.swift
+//  anipal 1
+//
+//  Created by Kim JoonOh on 2021/05/15.
+//
+
+import Foundation
+import UIKit
+
+struct Accessory {
+    var accessoryId: String
+    var imgUrl: String
+    var isOwn: Bool
+    var img: UIImage
+}
