e37ca7f834c8bf88bac9f0490c4dbad158ebe956
Joon <kjo11300@naver.com>
2021-09-19 21:28:17 +0900

    Feat: 싱글톤 animal 클래스 생성

--- Old/anipal 1/Model/animal.swift
+++ Old/anipal 1/Model/animal.swift
@@ -31,22 +31,20 @@ struct MyAnimal {
     var id: String
     var time: String
     var name: String
-    var animal: [String: String]
+    var animalUrl: [String: String]
+    var image: UIImage?
     
-    init(id: String, time: String, name: String, animal: [String: String]) {
+    init(id: String, time: String, name: String, animalUrl: [String: String]) {
         self.id = id
         self.time = time
         self.name = name
-        self.animal = animal
+        self.animalUrl = animalUrl
     }
 }
 
-struct MyAnimalInfo {
-    static let shared = MyAnimalInfo()
-    var id: String?
-    var time: String?
-    var name: String?
-    var image: UIImage?
+struct SingletonAnimal {
+    static let shared = SingletonAnimal()
+    var animal: [MyAnimal]?
 }
 
 struct Animal {
